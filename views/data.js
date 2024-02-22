3a587034c71fc5a8c51e14b143d865d7b25de09c
aristotelesbarros <143902784+aristotelesbarros@users.noreply.github.com>
2024-02-22 18:08:42 -0300

    att

---
+++ views/data.js
@@ -0,0 +1,25 @@
+module.exports=[
+    {
+        id:"iDsvCiVG9kk",
+        title:"CARROS NOVOS",
+        duration:"17:50 min",
+        price:"free",
+        featured: true
+
+    },
+    {
+        id:"lUq6TwYi8mg",
+        title:"CARROS SEMI NOVOS",
+        duration:"17:50",
+        price:"free"
+
+    },
+    {
+        id:"0813eXwgC6I",
+        title:"CARROS ANTIGOS",
+        duration:"17:50",
+        price:"free",
+        featured: true
+
+    }
+]
